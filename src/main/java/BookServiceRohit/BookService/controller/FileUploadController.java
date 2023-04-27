6953188584b3f7974777c448edf6dec58a500afa
rohit <rohitpushp123@gmail.com>
2023-04-27 09:46:50 +0530

    Bank service

--- src/main/java/BookServiceRohit/BookService/controller/FileUploadController.java
+++ src/main/java/BookServiceRohit/BookService/controller/FileUploadController.java
@@ -4,10 +4,11 @@ import BookServiceRohit.BookService.helper.FileUploadhelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.File;
 
 @RestController
 public class FileUploadController {
@@ -39,7 +40,8 @@ public class FileUploadController {
            boolean f=fileUploadhelper.uploadFile(file);
         if (f)
         {
-            return ResponseEntity.ok("File Successfully uploaded");
+             return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/").path(file.getOriginalFilename()).toUriString());// yeh path return krne ke liye hai(output is ) ...like.... http://localhost:1010/photo.jpg
+           // return ResponseEntity.ok("file successfully uploaded");
         }
 
        }catch (Exception e){
@@ -48,4 +50,5 @@ public class FileUploadController {
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong ! Try agrain......");
     }
+
 }
--- src/main/java/BookServiceRohit/BookService/helper/FileUploadhelper.java
+++ src/main/java/BookServiceRohit/BookService/helper/FileUploadhelper.java
@@ -1,11 +1,11 @@
 package BookServiceRohit.BookService.helper;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -13,7 +13,13 @@ import java.nio.file.StandardCopyOption;
 @Component
 public class FileUploadhelper
 {
-    public final String UPLOAD_DIR="C:\\Users\\rohit\\Downloads\\BookService\\src\\main\\resources\\static\\image";
+//    public final String UPLOAD_DIR="C:\\Users\\rohit\\Downloads\\BookService\\src\\main\\resources\\static\\image";
+    public final String UPLOAD_DIR=new ClassPathResource("static/image/").getFile().getAbsolutePath();
+    public FileUploadhelper() throws IOException
+    {
+
+
+    }
 
     public boolean uploadFile(MultipartFile multipartFile)
     {
@@ -38,4 +44,5 @@ public class FileUploadhelper
         }
         return f;
     }
+
 }
